9c535dfc309904cb270b959490534ad74ab5899a
fadlialvindra <38517667+fadlialvindra@users.noreply.github.com>
2018-05-22 04:04:03 +0700

    Update Tugas_Sorting.cpp

--- Tugas_Sorting.cpp
+++ Tugas_Sorting.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
 using namespace std;
-void buble () 
-{ 
-	int i;
-	int n;
-	cout<<"Jumlah elemen = ";
-	cin>>n;
-	int a_acak [n];
-	cout<<"Masukkan angka yang akan diurutkan = ";
-	for (i=0;i<n;i++){
-	cin>> a_acak [i]; 
+
+void selectSort();
+void bubbleSort();
+void mergeSort();
+void print();
+
+int data[100],n;
+int main ()
+{
+	int p;
+	cout<<"Masukkan Banyak data : "; cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Data ke-"<<i+1<<" : "; cin>>data[i];
 	}
-	
-	
-	int tmp;
-	for (int j= n-1;j>0;j--) {
-		for (int k =0;k<j;k++) {
-			if (a_acak[k] > a_acak[k+1]) {
-				tmp         = a_acak[k];
-				a_acak[k]   = a_acak[k+1];
-				a_acak[k+1] = tmp; 
-			}
+	cout<<"\nData sebelum Sorting : "; print();
+	ulang:
+	cout<<"\nPilih Sorting\n1.Selection Sort\n2.Bubble Sort\n3.Merge Sort\nMasukkan Pilihan : ";
+	cin>>p;
+	switch(p)
+	{
+		case 1: selectSort(); cout<<"\nData sesudah Sorting : "; print(); break;
+		case 2: bubbleSort(); cout<<"\nData sesudah Sorting : "; print(); break;
+		case 3: mergeSort(); cout<<"\nData sesudah Sorting : "; print(); break;
+		default: cout<<"Pilihan tidak ada\n"; goto ulang;;
+	}
+	return 0;
+}
+
+void selectSort()
+{
+	for(int i=0;i<n;i++)
+	{
+		int min=i;
+
+		for(int j=i+1;j<n;j++)
+		{
+			if(data[min]>data[j])
+				min=j;
 		}
-	}
-	cout<<"Hasil Bubble Sorting = " ;
-	for (int l=0;l<n;l++){
-	cout<<a_acak [l]<<", ";
+
+		if(min!=i)
+		{
+			int temp=data[i];
+			data[i]=data[min];
+			data[min]=temp;
+		}
 	}
 }
 
-int main () {
-	int p;
-	cout<<"Pilih Metode Sorting =";
-	cin>>p;
-	switch (p) {
-		case 1 : cout<<"Selection Sorting";
-			break;
-		case 2 : cout<<"Bubble Sorting\n";
-		buble ();
-			break;
-		case 3 : cout<<"Merge Sorting";
-			break;
+void bubbleSort()
+{
+	
+}
+
+void mergeSort()
+{
+	
+}
+
+void print()
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<data[i]<<" ";
 	}
+	cout<<endl;
 }
