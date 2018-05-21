d9221026faacaa7950c9fcb9678fdb4f299e1c44
1717051042-AdindaRizkyFebiyanto <38020024+SyncArf99@users.noreply.github.com>
2018-05-22 04:52:38 +0700

    Update Tugas_Sorting.cpp

--- Tugas_Sorting.cpp
+++ Tugas_Sorting.cpp
@@ -52,7 +52,18 @@ void selectSort()
 
 void bubbleSort()
 {
-	
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(data[j]>data[j+1])
+			{
+				int temp=data[j];
+				data[j]=data[j+1];
+				data[j+1]=temp;
+			}
+		}
+	}
 }
 
 void mergeSort()
